48348c91faaf1e5e0f17b05dd9816f9ed93ce027
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2017-06-20 20:01:35 -0500

    Initial Commit

---
+++ Tap Tap Too/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tap Tap Too
+//
+//  Created by Alejandro Zamudio Guajardo on 6/20/17.
+//  Copyright © 2017 Adamant Jumper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tap Tap Too/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tap Tap Too
+//
+//  Created by Alejandro Zamudio Guajardo on 6/20/17.
+//  Copyright © 2017 Adamant Jumper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
